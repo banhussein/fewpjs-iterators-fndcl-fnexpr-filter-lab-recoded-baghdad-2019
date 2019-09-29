e942f927c935d002a840b36900b6baff8c4f173e
ban hussein <banhussien97@gmail.com>
2019-09-29 20:26:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+let arrayToLowerCase = []
+
+function findMatching(drivers, string) {
+    let even = drivers.filter(s => {
+        return s.toLowerCase() == string.toLowerCase();
+    });
+
+    return even
+}
+
+
+function fuzzyMatch(drivers, string) {
+    let even = drivers.filter(s => {
+        return s.search(string);
+    });
+
+    return even
+}
+
+
+function matchName(drivers, string) {
+    let even = drivers.filter(s => {
+        return s.name == string;
+    });
+
+    return even
+}
